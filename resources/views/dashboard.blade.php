2bef77e7b83971660e2dc50f70696bcae76b43af
Nick Goedhart <goedhartnick@gmail.com>
2022-04-14 12:50:34 +0200

    Dashboard page edited.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            Welcome {{Auth::user()->name}}
+            <!--{{__("Welcome {Auth::user()->name}") }}-->
+            <!--{{ __('Dashboard') }}-->
         </h2>
     </x-slot>
 <style>
@@ -38,16 +40,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <h1 class="mx-10 mt-10 text-4xl font-bold">Dashboard</h1>
-                <h2 class="mx-10 mt-10 text-xl font-bold">Koptekst 1</h2>
+                <h2 class="mx-10 mt-10 text-xl font-bold">Regenval Januari</h2>
                 <div class="grid bg-gray-300 border-black border-2 grid-rows-3 sm:grid-rows-2 sm:grid-cols-3 sm:grid-flow-row grid-flow-col gap-4 mx-10">
                     <div class="sm:row-span-3 sm:col-span-2 mx-10 my-10 h-auto">
                             <div class="grid grid-rows-1 grid-col-10 grid-flow-col gap-4 h-full rotate-180 vertical-text-graph">
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">1</p></div>
-                                <div class="h-2/4 bg-blue-600"><p class="rotate-90">2</p></div>
-                                <div class="h-3/4 bg-blue-600"><p class="rotate-90">3</p></div>
+                                <div class="h-1/6 bg-blue-600"><p class="rotate-90">1</p></div>
+                                <div class="h-3/4 bg-blue-600"><p class="rotate-90">2</p></div>
+                                <div class="h-3/7 bg-blue-600"><p class="rotate-90">3</p></div>
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">4</p></div>
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">5</p></div>
-                                <div class="h-2/4 bg-blue-600"><p class="rotate-90">6</p></div>
+                                <div class="h-5/6 bg-blue-600"><p class="rotate-90">5</p></div>
+                                <div class="h-3/4 bg-blue-600"><p class="rotate-90">6</p></div>
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">7</p></div>
                                 <div class="h-3/4 bg-blue-600"><p class="rotate-90">8</p></div>
                                 <div class="h-4/4 bg-blue-600"><p class="rotate-90">9</p></div>
@@ -57,66 +59,66 @@
                     <div class="sm:col-span-1 mx-10 my-10 h-auto"><div class="my-pie-chart1"></div></div>
                     <div class="text-center mx-10 my-10 sm:row-span-2 col-span-1 h-auto">
                         <div class="grid bg-gray-300 grid-rows-2 sm:grid-rows-2 sm:grid-cols-2 sm:grid-flow-row grid-flow-col gap-4">
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Eerste Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Twede Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Derde Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Vierde Kwart</h2></div>
                         </div>
                     </div>
                 </div>
-                <h2 class="mx-10 mt-10 text-xl font-bold">Koptekst 2</h2>
+                <h2 class="mx-10 mt-10 text-xl font-bold">Waterverbruik Januari</h2>
                 <div class="grid bg-gray-300 border-black border-2 grid-rows-3 sm:grid-rows-2 sm:grid-cols-3 sm:grid-flow-row grid-flow-col gap-4 mx-10">
                     <div class="sm:row-span-3 sm:col-span-2 mx-10 my-10 h-auto">
                             <div class="grid grid-rows-1 grid-col-10 grid-flow-col gap-4 h-full rotate-180 vertical-text-graph">
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">1</p></div>
                                 <div class="h-2/4 bg-blue-600"><p class="rotate-90">2</p></div>
                                 <div class="h-3/4 bg-blue-600"><p class="rotate-90">3</p></div>
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">4</p></div>
+                                <div class="h-5/6 bg-blue-600"><p class="rotate-90">4</p></div>
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">5</p></div>
                                 <div class="h-2/4 bg-blue-600"><p class="rotate-90">6</p></div>
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">7</p></div>
+                                <div class="h-3/6 bg-blue-600"><p class="rotate-90">7</p></div>
                                 <div class="h-3/4 bg-blue-600"><p class="rotate-90">8</p></div>
-                                <div class="h-4/4 bg-blue-600"><p class="rotate-90">9</p></div>
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">10</p></div>
+                                <div class="h-2/4 bg-blue-600"><p class="rotate-90">9</p></div>
+                                <div class="h-6/7 bg-blue-600"><p class="rotate-90">10</p></div>
                             </div>
                     </div>
                     <div class="sm:col-span-1 mx-10 my-10 h-auto"><div class="my-pie-chart2"></div></div>
                     <div class="text-center mx-10 my-10 sm:row-span-2 col-span-1 h-auto">
                         <div class="grid bg-gray-300 grid-rows-2 sm:grid-rows-2 sm:grid-cols-2 sm:grid-flow-row grid-flow-col gap-4">
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Eerste Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Twede Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Derde Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Vierde Kwart</h2></div>
                         </div>
                     </div>
                 </div>
-                <h2 class="mx-10 mt-10 text-xl font-bold">Koptekst 3</h2>
+                <h2 class="mx-10 mt-10 text-xl font-bold">Regenval Februari</h2>
                 <div class="grid bg-gray-300 border-black border-2 grid-rows-3 sm:grid-rows-2 sm:grid-cols-3 sm:grid-flow-row grid-flow-col gap-4 mx-10">
                     <div class="sm:row-span-3 sm:col-span-2 mx-10 my-10 h-auto">
                             <div class="grid grid-rows-1 grid-col-10 grid-flow-col gap-4 h-full rotate-180 vertical-text-graph">
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">1</p></div>
                                 <div class="h-2/4 bg-blue-600"><p class="rotate-90">2</p></div>
-                                <div class="h-3/4 bg-blue-600"><p class="rotate-90">3</p></div>
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">4</p></div>
+                                <div class="h-2/5 bg-blue-600"><p class="rotate-90">3</p></div>
+                                <div class="h-5/6 bg-blue-600"><p class="rotate-90">4</p></div>
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">5</p></div>
-                                <div class="h-2/4 bg-blue-600"><p class="rotate-90">6</p></div>
+                                <div class="h-1/6 bg-blue-600"><p class="rotate-90">6</p></div>
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">7</p></div>
-                                <div class="h-3/4 bg-blue-600"><p class="rotate-90">8</p></div>
+                                <div class="h-5/6 bg-blue-600"><p class="rotate-90">8</p></div>
                                 <div class="h-4/4 bg-blue-600"><p class="rotate-90">9</p></div>
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">10</p></div>
+                                <div class="h-1/6 bg-blue-600"><p class="rotate-90">10</p></div>
                             </div>
                     </div>
                     <div class="sm:col-span-1 mx-10 my-10 h-auto"><div class="my-pie-chart3"></div></div>
                     <div class="text-center mx-10 my-10 sm:row-span-2 col-span-1 h-auto">
                         <div class="grid bg-gray-300 grid-rows-2 sm:grid-rows-2 sm:grid-cols-2 sm:grid-flow-row grid-flow-col gap-4">
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Eerste Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Twede Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Derde Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Vierde Kwart</h2></div>
                         </div>
                     </div>
                 </div>
-                <h2 class="mx-10 mt-10 text-xl font-bold">Koptekst 4</h2>
+                <h2 class="mx-10 mt-10 text-xl font-bold">Waterverbruik Februari</h2>
                 <div class="grid bg-gray-300 border-black border-2 grid-rows-3 sm:grid-rows-2 sm:grid-cols-3 sm:grid-flow-row grid-flow-col gap-4 mx-10">
                     <div class="sm:row-span-3 sm:col-span-2 mx-10 my-10 h-auto">
                             <div class="grid grid-rows-1 grid-col-10 grid-flow-col gap-4 h-full rotate-180 vertical-text-graph">
@@ -126,8 +128,8 @@
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">4</p></div>
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">5</p></div>
                                 <div class="h-2/4 bg-blue-600"><p class="rotate-90">6</p></div>
-                                <div class="h-1/4 bg-blue-600"><p class="rotate-90">7</p></div>
-                                <div class="h-3/4 bg-blue-600"><p class="rotate-90">8</p></div>
+                                <div class="h-3/4 bg-blue-600"><p class="rotate-90">7</p></div>
+                                <div class="h-5/6 bg-blue-600"><p class="rotate-90">8</p></div>
                                 <div class="h-4/4 bg-blue-600"><p class="rotate-90">9</p></div>
                                 <div class="h-1/4 bg-blue-600"><p class="rotate-90">10</p></div>
                             </div>
@@ -135,10 +137,10 @@
                     <div class="sm:col-span-1 mx-10 my-10 h-auto"><div class="my-pie-chart4"></div></div>
                     <div class="text-center mx-10 my-10 sm:row-span-2 col-span-1 h-auto">
                         <div class="grid bg-gray-300 grid-rows-2 sm:grid-rows-2 sm:grid-cols-2 sm:grid-flow-row grid-flow-col gap-4">
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
-                        <div class="border-2 border-black"><h2 class="text-xl font-bold">koptekst</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Eerste Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Tweede Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Derde Kwart</h2></div>
+                        <div class="border-2 border-black"><h2 class="text-xl font-bold">Vierde Kwart</h2></div>
                         </div>
                     </div>
                 </div>
